fbfd4ddee5edaf943a7f1dbff3b3259a10f32393
amandaSribeiro <amanda.524.ribeiro@gmail.com>
2024-08-15 20:59:14 -0300

    Modificando arquivos de lugar

---
+++ src/js/songs.js
@@ -0,0 +1,13 @@
+export default[
+{
+    src: '../audio/twenty one pilots_ Stressed Out.mp3',
+    name: 'Stressed Out',
+    band: 'Twenty One Pilots'
+},
+
+{
+    src: '../audio/NSYNC - Bye Bye Bye.mp3',
+    name: 'Bye Bye Bye',
+    band: 'NSYNC',
+}
+];
